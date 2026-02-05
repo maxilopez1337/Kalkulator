a6e380b9d5021efee13c92c4344ceba79cd48e3d
maxilopez1337 <maciej.hagno@gmail.com>
2026-02-05 11:23:25 +0100

    Delete _archive/copy-of-stratton-prime-proposal directory

--- _archive/copy-of-stratton-prime-proposal/utils/format.ts
+++
@@ -1,7 +0,0 @@
-export const formatCurrency = (value: number): string => {
-  return new Intl.NumberFormat('pl-PL', {
-    style: 'currency',
-    currency: 'PLN',
-    minimumFractionDigits: 2
-  }).format(value);
-};
